234b2d04a7fd3e13dcc9ac46cee0190ab758060f
pglad5 <pglad5@wgu.edu>
2024-04-28 17:49:49 -0600

    update

--- main.py
+++ main.py
@@ -83,14 +83,14 @@ def go(config: DictConfig):
             # Implement here #
             ##################
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/test_regression_model",
+                f"{config['main']['components_repository']}/train_val_test_split",
                 'main',
                 parameters = {
                 "input": "clean_sample.csv:latest",
                 "test_size": config['modeling']['test_size'],
                 "random_seed": config['modeling']['random_seed'],
                 "stratify_by": config['modeling']['stratify_by']
-                }
+                },
             )
     
 
